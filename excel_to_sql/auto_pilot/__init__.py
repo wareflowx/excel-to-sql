294c8f008772a5ba58e8ce164fa8a87da43c0726
AliiiBenn <davidddpereiraaa6@gmail.com>
2026-01-22 09:31:52 +0100

    feat: export QualityScorer from auto_pilot module

--- excel_to_sql/auto_pilot/__init__.py
+++ excel_to_sql/auto_pilot/__init__.py
@@ -6,5 +6,6 @@ for automatically importing Excel files into SQLite databases.
 """
 
 from excel_to_sql.auto_pilot.detector import PatternDetector
+from excel_to_sql.auto_pilot.scorer import QualityScorer
 
-__all__ = ["PatternDetector"]
+__all__ = ["PatternDetector", "QualityScorer"]
